72fb8502de8378884d7cd77c7b918b426be18822
ofer <oferc008@gmail.com>
2023-08-08 19:55:36 +0300

    added login + register + logout

--- libary_shop/urls.py
+++ libary_shop/urls.py
@@ -4,7 +4,13 @@ from libary_shop import views
 
 urlpatterns = [
     path('', views.books, name='all_books'),
-    path('/Books_available_for_rent', views.Books_available_for_rent, name='Books_available'),
-    path('/Books_not_on_rent', views.Books_not_on_rent, name='Books_rent'),
+    path('Books_available_for_rent/', views.books_available, name='Books_available'),
+    path('Books_not_on_rent/', views.books_not_on_rent, name='Books_rent'),
+    path('login/', views.my_login, name='login'),
+    path('logout/', views.user_logout, name='user_logout'), 
+    path('register/', views.register, name='register'),
+    path('registration_failed/', views.registration_failed, name='registration_failed'),
+
+
 
 ]
--- libary_shop/views.py
+++ libary_shop/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
 from django.utils import timezone
 from libary_shop.models import Book,Loan,CustomUser
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -14,7 +17,7 @@ def books(request):
     return render(request, 'index.html', {'all_books': all_books})
 
 
-def Books_not_on_rent(request):
+def books_not_on_rent(request):
     # Fetch all loans that are currently active (loan_date is not in the future and return_date is null or in the future)
     active_loans = Loan.objects.filter(loan_date__lte=timezone.now(), return_date__gte=timezone.now())
 
@@ -24,7 +27,7 @@ def Books_not_on_rent(request):
     # Pass the active_books to the template
     return render(request, 'books_not_on_rent.html', {'books_not_on_rent': books_not_on_rent})
 
-def Books_available_for_rent(request):
+def books_available(request):
     # Fetch all loans that are currently active (loan_date is not in the future and return_date is null or in the future)
     active_loans = Loan.objects.filter(loan_date__lte=timezone.now(), return_date__gte=timezone.now())
 
@@ -36,3 +39,63 @@ def Books_available_for_rent(request):
 
     # Pass the books_not_on_rent to the template
     return render(request, 'active_books.html', {'active_books': active_books})
+
+
+def my_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            print(f"** login passed. user is:{user}")
+            return redirect('all_books')  # Redirect to the homepage after successful login
+        else:
+            print(f"!! error login. user is:{user}")
+            # If authentication fails, show an error message or redirect back to the login page
+            error_message = "Invalid credentials. Please try again."
+            return render(request, 'index.html', {'error_message': error_message})
+    return render(request, 'login.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('all_books')
+
+
+
+
+def register(request):
+    if request.method == 'POST':
+        new_username = request.POST.get('new_username')
+        new_password = request.POST.get('new_password')
+        city = request.POST.get('city')
+        age = request.POST.get('age')
+
+        if new_username and new_password and city and age:
+            try:
+                # Check if the username is already taken
+                existing_user = CustomUser.objects.get(username=new_username)
+                # If user with the same username exists, raise an exception
+                raise ValueError('The username is already taken.')
+            except CustomUser.DoesNotExist:
+                # If the user with the same username does not exist, create a new user
+                user = CustomUser.objects.create_user(username=new_username, password=new_password)
+                # Set additional fields for the user (city and age)
+                user.city = city
+                user.age = age
+                user.save()  # Save the user profile with the city and age
+
+                # Automatically log in the user after successful registration
+                login(request, user)
+                return redirect('all_books')  # Redirect to a page after successful registration
+            except Exception as e:
+                # Handle other exceptions, if any
+                return redirect('registration_failed')  # Redirect to a generic registration failed page
+
+    return render(request, 'register.html', {'user': request.user})
+
+def registration_failed(request):
+    return render(request, 'registration_failed.html')
